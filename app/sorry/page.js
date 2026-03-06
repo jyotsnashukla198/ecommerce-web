e0343be4bdf7128238368e303e60a9210116f230
jyotsnashukla198 <jyotsnashukla198@gmail.com>
2026-03-06 19:01:14 +0000

    flex css

---
+++ app/sorry/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function SorryPage(){
+    return(
+        <div className="sorryContainer">
+            <h2>We are sorry to go! Please visit our website</h2>
+            <div className="sorryContainerChild">
+              <img src="/uploads/1772765375264-wxr1kkr4jhj.jpg"></img>  
+            </div>      
+       </div>
+    )
+}

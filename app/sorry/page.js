3eaaa00e60fe6b24f1546953c73784970606b5d4
EC2 Default User <ec2-user@ip-10-0-16-53.ap-south-1.compute.internal>
2026-03-23 02:13:22 +0000

    auto scaling group permission fix

--- app/sorry/page.js
+++ app/sorry/page.js
@@ -55,7 +55,9 @@ export default function SorryPage(){
 
     return(
         <div className="sorryContainer">
-            <h2>We are sorry to go! Please visit our website.</h2>
+            <h2>We are sorry to go! Please visit our website.Hello
+
+            </h2>
             <div className="sorryContainerChild">
               <img src="/uploads/1772765375264-wxr1kkr4jhj.jpg"></img>  
               <img  ref={containerRef} src="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=1920"></img> 
